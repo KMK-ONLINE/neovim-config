74ee7b4dedeba952b3b65aa39269b6effa022653
Ahmad Tanwir <mail@ata.web.id>
2015-10-16 17:32:07 +0700

    New baby is born

---
+++ README.md
@@ -0,0 +1,6 @@
+# neovim-config
+
+## Quick Install
+```
+wget -O - https://raw.githubusercontent.com/KMK-ONLINE/neovim-config/master/install.sh | bash
+```
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export NEOVIM_REPO="https://github.com/KMK-ONLINE/neovim-config.git"
+export NEOVIM_DIR="${HOME}/.neovim"
+export VUNDLE_DIR="${NEOVIM_DIR}/bundle/Vundle.vim"
+export VUNDLE_REPO="https://github.com/VundleVim/Vundle.vim"
+
+# fix some flacky when on home
+cd /tmp
+
+# clean up
+if [ -d $NEOVIM_DIR ]; then
+  rm -rf $NEOVIM_DIR
+fi
+
+git clone $NEOVIM_REPO $NEOVIM_DIR;
+git clone $VUNDLE_REPO $VUNDLE_DIR;
+
+# create symlinks
+ln -sf "${NEOVIM_DIR}/.nvimrc" ~/.nvimrc
+
+# Install bundle with vundle
+nvim +PluginInstall +qall
